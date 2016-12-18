5891b725491dddbe5f948b25d514ef332bffff7e
Alex Lementuev <a.lementuev@gmail.com>
2016-12-18 00:59:36 -0800

    Added ‘Cubes9'

--- Assets/Scripts/Processing/Sketch.Environment.cs
+++ Assets/Scripts/Processing/Sketch.Environment.cs
@@ -34,7 +34,19 @@ public partial class Sketch
         _height = height;
     }
 
-    protected void smooth() { throw new NotImplementedException(); }
+    /// <summary>
+    /// Draws all geometry with smooth (anti-aliased) edges. This behavior is the default, so smooth() only needs to be used when a program needs to set the smoothing in a different way. The level parameter increases the level of smoothness. This is the level of over sampling applied to the graphics buffer.
+    /// 
+    /// With the P2D and P3D renderers, smooth(2) is the default, this is called "2x anti-aliasing." The code smooth(4) is used for 4x anti-aliasing and smooth(8) is specified for 8x anti-aliasing. The maximum anti-aliasing level is determined by the hardware of the machine that is running the software, so smooth(4) and smooth(8) will not work with every computer.
+    ///
+    /// The default renderer uses smooth(3) by default. This is bicubic smoothing. The other option for the default renderer is smooth(2), which is bilinear smoothing.
+    ///
+    /// With Processing 3.0, smooth() is different than before. It was common to use smooth() and noSmooth() to turn on and off antialiasing within a sketch. Now, because of how the software has changed, smooth() can only be set once within a sketch. It can be used either at the top of a sketch without a setup(), or after the size() function when used in a sketch with setup(). The noSmooth() function also follows the same rules.
+    /// </summary>
+    protected void smooth(int level)
+    {
+        warning("smooth(level)");
+    }
 
     #region Properties
 
---
+++ Assets/Scripts/Sketches/Cubes/Cubes9.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cubes9 : Sketch
+{
+    bool colored = true;
+
+    protected override void setup()
+    {
+        size(500, 500, P3D);
+        colorMode(HSB, 1);
+        frameRate(24);
+        stroke(0, 0, colored ? 0.4f : 0);
+        smooth(4);
+    }
+
+    protected override void draw()
+    {
+        int totalFrames = 24 * 4;
+        float time = 1.0f / totalFrames * frameCount;
+
+        background(0.13f, colored ? 0.07f : 0.03f, 1);
+
+        perspective(0.5f, 1, 0.01f, 100);
+
+        camera(
+            0, 0, 32 + sin(PI * 2 * time) * 5,
+            0, 0, 0,
+            0, 1, 0
+        );
+
+        rotateX(-0.8f + 0.1f * cos(PI * 2 * time));
+        rotateZ( 0.8f + 0.1f * sin(PI * 2 * time));
+
+        const int slice = 24;
+        const int ring = 44;
+        const int radius = 8;
+
+        int id = 0;
+        for (int i = 0; i <= slice; i++)
+        {
+            float theta = (1.0f * i / slice) % 1;
+            theta = PI * (theta - 0.5f);
+
+            for (int j = 0; j < ring; j++)
+            {
+                float phi = 2 * PI * j / ring;
+
+                float s = cos(theta);
+                float l = radius;
+
+                s *= abs(abs(1.0f * id / 24 + time * 6) % 2 - 1);
+                s *= 1.15f;
+
+                if (colored) fill(s * 0.28f + 0.17f, 0.4f, 1);
+
+                pushMatrix();
+                rotateY(phi);
+                rotateX(-theta);
+                translate(0, 0, l);
+                box(s, s, s);
+                popMatrix();
+
+                id++;
+            }
+        }
+
+        // if (frameCount <= totalFrames) saveFrame();
+    }
+}
