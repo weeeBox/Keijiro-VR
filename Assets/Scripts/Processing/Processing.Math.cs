0a8c8623ab6e83b74037e0c053fa6a183bf4592b
Alex Lementuev <a.lementuev@gmail.com>
2016-12-17 20:31:14 -0800

    Added ‘Cubes4’ and Perlin noise

--- Assets/Scripts/Processing/Processing.Math.cs
+++ Assets/Scripts/Processing/Processing.Math.cs
@@ -32,7 +32,28 @@ public partial class Processing
         return (int)Mathf.Ceil(n);
     }
 
-    //    constrain()
+    /// <summary>
+    /// Constrains a value to not exceed a maximum and minimum value.
+    /// </summary>
+    /// <param name="amt">the value to constrain</param>
+    /// <param name="low">minimum limit</param>
+    /// <param name="high">maximum limit</param>
+    protected float constrain(float amt, float low, float high)
+    {
+        return Mathf.Clamp(amt, low, high);
+    }
+
+    /// <summary>
+    /// Constrains a value to not exceed a maximum and minimum value.
+    /// </summary>
+    /// <param name="amt">the value to constrain</param>
+    /// <param name="low">minimum limit</param>
+    /// <param name="high">maximum limit</param>
+    protected int constrain(int amt, int low, int high)
+    {
+        return Mathf.Clamp(amt, low, high);
+    }
+
     //    dist()
     //    exp()
     //    floor()
@@ -98,6 +119,44 @@ public partial class Processing
     }
 
     //    tan()
+
+    #endregion
+
+    #region Random
+
+    /// <summary>
+    /// Returns the Perlin noise value at specified coordinates. Perlin noise is a random sequence generator producing a more natural, harmonic succession of numbers than that of the standard random() function. It was developed by Ken Perlin in the 1980s and has been used in graphical applications to generate procedural textures, shapes, terrains, and other seemingly organic forms.
+    /// 
+    /// In contrast to the random() function, Perlin noise is defined in an infinite n-dimensional space, in which each pair of coordinates corresponds to a fixed semi-random value (fixed only for the lifespan of the program). The resulting value will always be between 0.0 and 1.0. Processing can compute 1D, 2D and 3D noise, depending on the number of coordinates given. The noise value can be animated by moving through the noise space, as demonstrated in the first example above. The 2nd and 3rd dimensions can also be interpreted as time.
+    /// 
+    /// The actual noise structure is similar to that of an audio signal, in respect to the function's use of frequencies. Similar to the concept of harmonics in physics, Perlin noise is computed over several octaves which are added together for the final result.
+    /// 
+    /// Another way to adjust the character of the resulting sequence is the scale of the input coordinates. As the function works within an infinite space, the value of the coordinates doesn't matter as such; only the distance between successive coordinates is important (such as when using noise() within a loop). As a general rule, the smaller the difference between coordinates, the smoother the resulting noise sequence. Steps of 0.005-0.03 work best for most applications, but this will differ depending on use.
+    /// 
+    /// There have been debates over the accuracy of the implementation of noise in Processing. For clarification, it's an implementation of "classic Perlin noise" from 1983, and not the newer "simplex noise" method from 2001.
+    /// </summary>
+    protected float noise(float x, float y = 0, float z = 0)
+    {
+        return PerlinNoise.Noise(x, y, z);
+    }
+
+    /// <summary>
+    /// Adjusts the character and level of detail produced by the Perlin noise function. Similar to harmonics in physics, noise is computed over several octaves. Lower octaves contribute more to the output signal and as such define the overal intensity of the noise, whereas higher octaves create finer-grained details in the noise sequence.
+    ///
+    /// By default, noise is computed over 4 octaves with each octave contributing exactly half than its predecessor, starting at 50% strength for the first octave. This falloff amount can be changed by adding an additional function parameter. For example, a falloff factor of 0.75 means each octave will now have 75% impact (25% less) of the previous lower octave. While any number between 0.0 and 1.0 is valid, note that values greater than 0.5 may result in noise() returning values greater than 1.0.
+    ///
+    /// By changing these parameters, the signal created by the noise() function can be adapted to fit very specific needs and characteristics.
+    /// </summary>
+    /// <param name="lod">number of octaves to be used by the noise</param>
+    protected void noiseDetail(int lod)
+    {
+        warning("noiseDetail(lod)");
+    }
+
+    // noiseSeed()
+    // random()
+    // randomGaussian()
+    // randomSeed()
 
     #endregion
 }
---
+++ Assets/Scripts/Sketches/Cubes4.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cubes4 : Processing
+{
+    protected override void setup()
+    {
+        colorMode(RGB, 1);
+        size(500, 500, P3D);
+        frameRate(24);
+    }
+
+    protected override void draw()
+    {
+        int frames = 24 * 3;
+        float t = (float)frameCount / frames;
+        float t2 = t - 1;
+
+        noiseDetail(1);
+
+        background(1);
+
+        perspective(0.5f, 1, 0.01f, 100);
+
+        camera(
+            0, 0, 25 + sin(PI * 2 * t) * 3,
+            0, 0, 0,
+            0, 1, 0
+        );
+
+        rotateX(-0.5f - 0.05f * sin(PI * 4 * t));
+        rotateY(-0.5f - 0.05f * cos(PI * 2 * t));
+
+        int columns = 8;
+        float freq = 0.5f;
+        for (int ix = 0; ix < columns; ix++)
+        {
+            float x = ix - 0.5f * columns + 0.5f;
+            for (int iy = 0; iy < columns; iy++)
+            {
+                float y = iy - 0.5f * columns + 0.5f;
+                for (int iz = 0; iz < columns; iz++)
+                {
+                    float z = iz - 0.5f * columns + 0.5f;
+
+                    float n1 = noise(x * freq + t  * 6, y * freq, z * freq + t  * 2);
+                    float n2 = noise(x * freq + t2 * 6, y * freq, z * freq + t2 * 2);
+                    float s = constrain(lerp(n1, n2, t) * 6 - 1.5f, 0, 1);
+
+                    pushMatrix();
+                    translate(x, z, y);
+                    box(s);
+                    popMatrix();
+                }
+            }
+        }
+
+        //if (frameCount <= frames) saveFrame();
+    }
+}
